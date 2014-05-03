7026ad31edf6e107480bf7b694c6fad78957ef6d
John Doe <johndoe@example.com>
2014-05-03 15:46:17 +0800

    show user_name in admin welcome

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,8 @@
 class AdminController < ApplicationController
   def welcome
     @name = User.find(session[:user_id]).name
+    @users = User.where(:admin => nil)
+    p ".........................."
+    p @users
   end
 end
