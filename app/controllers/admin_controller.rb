3e51091237b721bf3c2b54c2fd7b6debba1001f0
John Doe <johndoe@example.com>
2014-05-04 13:46:08 +0800

    modify << >> css

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,6 @@
 class AdminController < ApplicationController
   def welcome
     @name = User.find(session[:user_id]).name
-    #@users = User.where(:admin => nil)
     @users = User.where(:admin => nil).paginate(page: params[:page],:per_page => 10)
-    @count = @users.length/10+1
   end
 end
