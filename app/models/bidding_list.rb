b12557b9048adf10fbfc110b0172e16c6d6a5c04
John Doe <johndoe@example.com>
2014-05-22 13:30:41 +0800

    add deal with without sign_up bidding

--- app/models/bidding_list.rb
+++ app/models/bidding_list.rb
@@ -1,8 +1,10 @@
 class BiddingList < ActiveRecord::Base
   def self.update_bidding_list(user,bid_list)
     BiddingList.delete_all(:user_name => user)
-    bid_list.each do |key,value|
-      BiddingList.create(:user_name => user,:activity_name => value[:activity_name],:bid_name => value[:name],:sign_up_counts => value[:sign_up_counts],:bidding_counts => value[:bidding_counts])
+    if bid_list.present?
+      bid_list.each do |key,value|
+        BiddingList.create(:user_name => user,:activity_name => value[:activity_name],:bid_name => value[:name],:sign_up_counts => value[:sign_up_counts],:bidding_counts => value[:bidding_counts])
+      end
     end
   end
 end
--- app/models/sign_up_list.rb
+++ app/models/sign_up_list.rb
@@ -1,9 +1,13 @@
 class SignUpList < ActiveRecord::Base
   def self.update_sign_up_list(user,sign_up_list)
     SignUpList.delete_all(:user_name => user)
-    sign_up_list.each do |key,value|
-      value[:applicants].each do |k,val|
-        SignUpList.create(:user_name => user,:activity_name => value[:activity_name],:name => val[:name],:phone => val[:phone])
+    if sign_up_list.present?
+      sign_up_list.each do |key,value|
+        if value[:applicants].present?
+          value[:applicants].each do |k,val|
+            SignUpList.create(:user_name => user,:activity_name => value[:activity_name],:name => val[:name],:phone => val[:phone])
+          end
+        end
       end
     end
   end
