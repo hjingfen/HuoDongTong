b4e30cfd292d7895dffaa5ef1cdd1d70dcbe33f2
John Doe <johndoe@example.com>
2014-06-05 10:29:53 +0800

    add alert of use_name password can not be null

--- public/www/js/controller/LoginController.js
+++ public/www/js/controller/LoginController.js
@@ -1,5 +1,13 @@
 function LoginController($scope,$navigate,$http){
     $scope.user_login = function(){
+        $scope.user_name = $scope.user_name || '';
+        $scope.password = $scope.password || '';
+        $scope.login_tip = $scope.login_with_null = false;
+        if ($scope.user_name == '' || $scope.password == '')
+        {
+            $scope.login_with_null = true;
+            return;
+        }
         var data = {user_name:$scope.user_name, password:$scope.password};
         $http.post('/phone_login',data).success(function(return_data){
            if(return_data=='true'){
